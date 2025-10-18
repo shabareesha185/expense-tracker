825f823b331c4b4be1a7afac9fa4a2e0763d1eeb
Shabareesha <arjunspy491@gmail.com>
2025-10-18 15:42:09 +0530

    UI

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let inputExpenceName =  document.getElementById("input-expense");
+    let inputExpenceAmount =  document.getElementById("input-amount");
+})
